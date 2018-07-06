6081002fd9445f84b44ac1a9ebc5b9e8fa913d82
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2018-07-06 10:53:20 +0200

    Add support for assertions on jars (except for newer)

--- internal/zinc-scripted/src/test/scala/sbt/internal/inc/ZincFileCommands.scala
+++ internal/zinc-scripted/src/test/scala/sbt/internal/inc/ZincFileCommands.scala
@@ -1,6 +1,7 @@
 package sbt.internal.inc
 
 import java.io.File
+import java.net.URL
 
 import sbt.internal.scripted.FileCommands
 
@@ -14,5 +15,30 @@ class ZincFileCommands(baseDirectory: File) extends FileCommands(baseDirectory) 
         println("Restarting the execution.")
       }
     }
+  }
+
+  override def absent(paths: List[String]): Unit = {
+    val (jared, regular) = paths.partition(_.startsWith("jar:file"))
+    val jaredPresent = jared.exists(existsInJar)
+    val present = fromStrings(regular).filter(_.exists)
+    if (present.nonEmpty || jaredPresent)
+      scriptError("File(s) existed: " + present.mkString("[ ", " , ", " ]"))
+  }
+
+  override def newer(a: String, b: String): Unit = super.newer(a, b)
+
+  override def exists(paths: List[String]): Unit = {
+    val (jars, regular) = paths.partition(_.startsWith("jar:file"))
+    val jaredNotPresent = !jars.forall(existsInJar)
+    val notPresent = fromStrings(regular).filter(!_.exists)
+    if (notPresent.nonEmpty || jaredNotPresent)
+      scriptError("File(s) did not exist: " + notPresent.mkString("[ ", " , ", " ]"))
+  }
+
+  def existsInJar(url: String): Boolean = {
+    val Array(jarPath, filePath) = url.stripPrefix("jar:file").split("!")
+    val absJarPath = new File(baseDirectory, jarPath).getPath
+    val absoluteUrl = s"jar:file$absJarPath!$filePath"
+    new URL(absoluteUrl).openConnection().getContentLength >= 0
   }
 }
