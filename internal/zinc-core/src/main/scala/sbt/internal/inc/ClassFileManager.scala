ccbe74812490c83d9f8811ce2cc0b1a1ae51870c
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2018-07-13 14:52:04 +0200

    Extra jar for merging in cfm...

--- internal/zinc-core/src/main/scala/sbt/internal/inc/ClassFileManager.scala
+++ internal/zinc-core/src/main/scala/sbt/internal/inc/ClassFileManager.scala
@@ -181,7 +181,10 @@ object ClassFileManager {
         show(s"Restoring jared class files: \n${showFiles(movedJaredClasses.keys)}")
         toMove.foreach {
           case (srcJar, tmpJar) =>
-            STJUtil.mergeJars(into = srcJar, from = tmpJar)
+            val tmpSrc = tmpJar.toPath.resolveSibling("~~tmp~cfm~merge~~.jar").toFile
+            Files.copy(srcJar.toPath, tmpSrc.toPath)
+            STJUtil.mergeJars(into = tmpSrc, from = tmpJar)
+            Files.move(tmpSrc.toPath, srcJar.toPath)
         }
       }
       logger.debug(s"Removing the temporary directory used for backing up class files: $tempDir")
