aebce04718ec380fe73cc2726e2b4789f99d66d7
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2018-08-13 16:27:00 +0200

    Add temporary test to look at compiler performance

---
+++ zinc/src/sbt-test/source-dependencies/compilation-perf/src/main/scala/concurrent/Lock.scala
@@ -0,0 +1,30 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2013, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+
+
+package scala.concurrent
+
+/** This class ...
+ *
+ *  @author  Martin Odersky
+ */
+@deprecated("use java.util.concurrent.locks.Lock", "2.11.2")
+class Lock {
+  var available = true
+
+  def acquire() = synchronized {
+    while (!available) wait()
+    available = false
+  }
+
+  def release() = synchronized {
+    available = true
+    notify()
+  }
+}
